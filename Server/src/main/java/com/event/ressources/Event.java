3bcad90e905603be725308d1671446eeaa6e501b
LudovicRobez <ludovic.robez@hotmail.fr>
2019-03-15 10:51:15 +0100

    Ajout de la ressource event

---
+++ Server/src/main/java/com/event/ressources/Event.java
@@ -0,0 +1,80 @@
+package com.event.ressources;
+
+import com.user.ressources.User;
+
+import java.util.Date;
+
+public class Event {
+
+    private int id;
+    private String name;
+    private User user;
+    private Date date;
+    private long longitude;
+    private long latitude;
+
+    public Event(String name, User user, Date date, long longitude, long latitude) {
+        this.name = name;
+        this.user = user;
+        this.date = date;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Event(int id, String name, User user, Date date, long longitude, long latitude) {
+        this.id = id;
+        this.name = name;
+        this.user = user;
+        this.date = date;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+}
